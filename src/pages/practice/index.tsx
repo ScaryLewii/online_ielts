ed2dcf66e07abe6377a667428919dffb23de3f4d
ScaryLewii <120034859+ScaryLewii@users.noreply.github.com>
2023-07-26 06:28:18 +0700

    done practice

--- src/pages/practice/index.tsx
+++ src/pages/practice/index.tsx
@@ -2,14 +2,18 @@ import Breadcrumbs from "@/components/common/breadcrumbs"
 import CheckboxGroup from "./checkbox-group"
 import RadioGroup from "./radio-group"
 import TextAreaBox from "./textarea"
+import Footer from "./footer"
 
 const PracticePage = () => {
-	return <div>
-		<Breadcrumbs />
-		<RadioGroup />
-		<CheckboxGroup />
-		<TextAreaBox />
-	</div>
+	return <>
+		<div className="pb-[150px]">
+			<Breadcrumbs />
+			<RadioGroup />
+			<CheckboxGroup />
+			<TextAreaBox />
+		</div>
+		<Footer />
+	</>
 }
 
 export default PracticePage
