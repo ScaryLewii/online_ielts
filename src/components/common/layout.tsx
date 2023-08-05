5c7228ac293c90ac14a8ad9fcead7ecdbf496dfc
ScaryLewii <120034859+ScaryLewii@users.noreply.github.com>
2023-08-05 18:19:26 +0700

    fix bg + setting page

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -95,7 +95,7 @@ const Layout = observer(({ children }: PropsWithChildren) => {
 			<div className="dashboard-wrapper flex">
 				<SideNav />
 				<main className="bg-sea w-full min-h-screen relative" style={{gridArea: "dashboard"}}>
-					<Image src={dashboardbg} width={1920} height={1080} alt="background" loading="lazy" className="absolute top-0 left-0 z-0" />
+					<Image src={dashboardbg} width={1920} height={1080} alt="background" loading="lazy" className="absolute top-0 left-0 z-0 max-h-full" />
 					<TopNav />
 					<section className={`relative z-[1] ${router.asPath.includes("courses/quiz") ? "" : "p-5 xl:p-14"}`}>
 						{children}
--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -3,18 +3,6 @@ import { useRouter } from "next/router"
 import { useEffect } from "react"
 
 const SettingPage = () => {
-	const router = useRouter()
-	useEffect(() => {
-		const handleGetSession = async () => {
-			const session = await getSession()
-			if (!session) {
-				router.push('/signin')
-			}   
-		}
-		
-		handleGetSession()
-	})
-
 	return <div className="text-white lg:max-w-[600px]">
 		<div className="pb-10 border-b border-white">
 			<h2 className="text-xl font-semibold mb-8">Thông báo cho tôi khi....</h2>
