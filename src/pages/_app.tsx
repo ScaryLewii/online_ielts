a264068ca5a0eac2631e38c3856fa7587fe38d95
ScaryLewii <120034859+ScaryLewii@users.noreply.github.com>
2023-08-26 07:53:51 +0700

    improve cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import Layout from '@/components/common/layout'
 import '@/styles/globals.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app'
 
 const queryClient = new QueryClient();
@@ -8,9 +8,11 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
     </QueryClientProvider>
   )
 }
