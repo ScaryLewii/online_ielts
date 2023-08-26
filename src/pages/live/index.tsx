552f7934f86faedf0902f82e31c75723138b7b35
ScaryLewii <120034859+ScaryLewii@users.noreply.github.com>
2023-08-26 07:59:51 +0700

    fix live page UI

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -1,21 +1,16 @@
-import { getSession } from "next-auth/react"
-import { useRouter } from "next/router"
-import { useEffect } from "react"
+import PageNotFoundBackGround from "../../../public/images/404.svg"
+import Image from "next/image"
 
 const LivePage = () => {
-	const router = useRouter()
-	useEffect(() => {
-		const handleGetSession = async () => {
-			const session = await getSession()
-			if (!session) {
-				router.push('/signin')
-			}   
-		}
-		
-		handleGetSession()
-	})
-
-	return <h1>afasfasf</h1>
+	return <>
+		<h1 className="text-center text-white font-semibold mb-5">
+			XIN CHÀO, <br/>
+			CHÚNG MÌNH ĐANG CẬP NHẬT TÍNH NĂNG NÀY!
+		</h1>
+		<p className="text-white block max-w-[840px] mx-auto text-center">Bạn rất quan trọng với Ant Edu chúng mình, vậy nên đội ngũ Ant Edu đã và đang nhanh chóng cập nhật phiên bản mới nhất của tính năng này đến bạn.Hãy tiếp tục luyện tập chăm chỉ nhé, thông báo cập nhật thành công sẽ sớm được gửi đến bạn!</p>
+	
+		<Image src={PageNotFoundBackGround} width={450} height={450} alt="page not found" className="mx-auto mt-10" />
+	</>
 }
 
 export default LivePage
