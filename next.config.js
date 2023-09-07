cfa02342cf45055eea6fdcb02cae1e820b5bdb6e
ScaryLewii <120034859+ScaryLewii@users.noreply.github.com>
2023-09-07 09:13:31 +0700

    fix cache problem

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { buildId, dev }) => {
+    const newConfig = config;
+
+    if (!dev && newConfig.output.filename.startsWith('static')) {
+        newConfig.output.filename = newConfig.output.filename.replace('[name]', `[name]-${buildId}`);
+        newConfig.output.chunkFilename = newConfig.output.chunkFilename.replace('[name]', `[name]-${buildId}`);
+    }
+
+    return newConfig;
+  },
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
     return '2023-09-07-01'
