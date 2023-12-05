e7657cbd068dc55a69cac8af69e27ee7db0a101c
ScaryLewii <120034859+ScaryLewii@users.noreply.github.com>
2023-12-05 15:01:48 +0700

    adding categories link into route component

--- src/pages/study-route/route.tsx
+++ src/pages/study-route/route.tsx
@@ -11,13 +11,13 @@ import icon3 from "../../../public/dump/icon-3.svg"
 import routeMobile from "../../../public/images/route-mobile.svg"
 import route from "../../../public/images/route.svg"
 
-const PlainContent = () => {
+const PlainContent = ({url}: {url: string}) => {
 	return <>
 		<div className="hidden md:flex items-center gap-2 mt-5 opacity-0 group-hover:opacity-100 whitespace-nowrap">
-			<button className="px-5 py-2 border border-[#51C84E] rounded-full inline-flex gap-2 items-center">
+			<Link href={url} className="px-5 py-2 border border-[#51C84E] rounded-full inline-flex gap-2 items-center">
 				<ReactSVG src={icon1["src"]} width={24} height={24} />
 				Lý thuyết
-			</button>
+			</Link>
 			<button className="px-5 py-2 border border-[#51C84E] rounded-full inline-flex gap-2 items-center">
 				<ReactSVG src={icon2["src"]} width={24} height={24} />
 				Luyện tập
@@ -71,7 +71,7 @@ const Route = () => {
 					return (
 						<div key={nanoid()} className={`absolute cursor-pointer group ${Items[index].positionMb} ${Items[index].position}`}>
 							<Link href={`/categories/${cat.id}`} className={`${Items[index].color} font-semibold text-[18px] md:text-[22px] group-hover:text-cyan`}>{cat.name}</Link>
-							<PlainContent />
+							<PlainContent url={`/categories/${cat.id}`} />
 						</div>
 					)
 				}
