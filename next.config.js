e2e7ae98d9ef093e86b9563959fff3c0dbe65d4b
Thinh.Nguyen <thinh.nguyen@niteco.se>
2023-12-20 14:49:22 +0700

    update version

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
   },
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
-    return '2023-12-20-01'
+    return '2023-12-20-02'
   },
   reactStrictMode: true,
   async redirects() {
