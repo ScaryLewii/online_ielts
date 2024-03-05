1e6d4ab9a2e994646d3015b59d9f6820973cf2f9
TruongBL <truongblt@gmail.com>
2024-03-06 00:18:15 +0700

    update allcourse/course/index

--- src/pages/all-courses/course/[course_id]/index.tsx
+++ src/pages/all-courses/course/[course_id]/index.tsx
@@ -1,40 +1,29 @@
-
 "use client"
-
 import { useCourseQuery } from "@/base/query"
 import CourseDetail from "@/components/course/course-detail"
 import { GlobalContext } from "@/context/context"
 import CourseInfo from "@/pages/courses/course-info"
 import { useRouter } from "next/router"
-import { useContext, useEffect, useState } from "react"
+import { useContext } from "react"
+
 
 const CourseContent = () => {
-	const router = useRouter()
+	const { query, isReady } = useRouter()
 	const context = useContext(GlobalContext)
-	const courseId = router.query.course_id as string
-	const { isFetched: isFinishFetchCourse, data: courseData } = useCourseQuery(+courseId, context.cookies.get())
-	const [isClient, setIsClient] = useState(false);
+	const courseId = query.course_id as string
+	const { isFetched: isFetchedCourse, data: courseData } = useCourseQuery(+courseId, context.cookies.get())
 
-	useEffect(() => {
-		setIsClient(true)
-		console.log(courseId)
-	}, [])
+	if(!isReady || !isFetchedCourse) return <></>
 
-	if (!isFinishFetchCourse) return <></>
-
-	return <>
-		{isClient &&
-			<div className="flex gap-10 flex-wrap text-white p-5 xl:p-10 relative z-[1]">
-				<div className="w-full lg:w-auto lg:min-w-[550px] border border-white py-5 px-5">
-					{/* <CourseBox courseId={course.id} /> */}
-					<CourseDetail item={courseData} />
-				</div>
-				<div className=" xl:max-w-[450px]">
-					<CourseInfo videoUrl={courseData?.introVideo} />
-				</div>
-			</div>
-		}
-	</>
+	return <div className="flex gap-10 flex-wrap text-white p-5 xl:p-10 relative z-[1]">
+		<div className="w-full lg:w-auto lg:min-w-[550px] border border-white py-5 px-5">
+			{/* <CourseBox courseId={course.id} /> */}
+			<CourseDetail item={courseData} />
+		</div>
+		<div className=" xl:max-w-[450px]">
+			<CourseInfo videoUrl={courseData?.introVideo} />
+		</div>
+	</div>
 }
 
 export default CourseContent
